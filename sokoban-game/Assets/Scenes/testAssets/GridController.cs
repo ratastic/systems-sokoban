77163f45dc9c7dcedf66138f375c602565accc5d
vega jafri <giantwhitedeer@gmail.com>
2025-09-10 18:41:27 -0400

    demo stuff

---
+++ sokoban-game/Assets/Scenes/testAssets/GridController.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class GridController : MonoBehaviour
+{
+    public Tile block;
+    public Tile wall;
+    public Tile background;
+
+    public static GridController instance;
+    private Grid grid;
+    private Tilemap tilemap;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Awake()
+    {
+        if (instance != null) // if there is an instance in the scene already, destroy ourself
+        {
+            Destroy(this);
+            return;
+        }
+
+        instance = this; // set ourselves to singleton instance
+        grid = GetComponent<Grid>();
+        tilemap = transform.Find("blocks").GetComponent<Tilemap>();
+    }
+
+    public Vector3 GetWorldPos(int x, int y)
+    {
+        return grid.CellToWorld(new Vector3Int(x, y, 0));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public bool IsOccupied(int x, int y)
+    {
+        var tile = tilemap.GetTile(new Vector3Int(x, y, 0));
+        return tile != null && tile != block;
+    }
+
+    public void PushBlock(Vector3Int start, Vector3Int destination)
+    {
+        // set start position to the empty (background sprite)
+        tilemap.SetTile(start, background);
+        // set destination sprite to be the block
+        tilemap.SetTile(destination, block);
+    }
+}
+
+//public class Junk
+//{
+//    public void DoStuff()
+//    {
+//        GridController.instance.DoSomething();
+//    }
+
+//}
---
+++ sokoban-game/Assets/Scenes/testAssets/PlayerController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class PlayerController : MonoBehaviour
+{
+    private int x = 0;
+    private int y = 0;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        x = 0;
+        y = 0;
+        Move(0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Move(int xMove, int yMove)
+    {
+        x += xMove;
+        y += yMove;
+        transform.position = GridController.instance.GetWorldPos(x, y);
+
+    }
+}
