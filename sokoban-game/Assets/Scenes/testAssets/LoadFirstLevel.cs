043574d310c66b12624cdb00216b6dd1e1dd8f58
vega jafri <giantwhitedeer@gmail.com>
2025-10-01 04:19:23 -0400

    polishhh

---
+++ sokoban-game/Assets/Scenes/testAssets/LoadFirstLevel.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEditor.Toolbars;
+public class LoadFirstLevel : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void PressStart()
+    {
+        SceneManager.LoadScene("Level01");
+    }
+}
